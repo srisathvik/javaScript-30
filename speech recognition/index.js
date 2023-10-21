e66bb346a71ce2948e0a080f6c922de010f491bf
srisathvik <komminenisathvik@gmail.com>
2023-10-21 09:07:40 +0530

    Day 20

---
+++ speech recognition/index.js
@@ -0,0 +1,29 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement("p");
+const words = document.querySelector(".words");
+words.appendChild(p);
+
+recognition.addEventListener("result", (e) =>{
+    // console.log(e);
+    const transcript = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join("")
+
+    p.textContent = transcript;
+
+    if(e.result[0].isFinal){
+        p = document.createElement("p");
+        words.append(p);
+    }
+    // console.log(transcript);
+});
+
+recognition.addEventListener("end", recognition.start);
+recognition.start();
+
+recognition.start();
